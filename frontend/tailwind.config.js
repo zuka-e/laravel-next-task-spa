4e64867ca826a6c6b0d890d09ba5caf9d9d176f5
zuka-e <52391918+zuka-e@users.noreply.github.com>
2022-11-06 23:34:02 +0900

    Install TailwindCSS plugin `@tailwindcss/line-clamp`

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,7 +12,10 @@ module.exports = {
     // Remove Tailwind CSS's preflight style so it can use the MUI's preflight instead (CssBaseline).
     preflight: false,
   },
-  plugins: [],
+  plugins: [
+    // cf. https://github.com/tailwindlabs/tailwindcss-line-clamp
+    require('@tailwindcss/line-clamp'),
+  ],
 };
 
 // ※ If CSS isn't applied after `yarn dev`, maybe `.next/` should be removed.
