01ad764cae448c9f905ba331e1ce6077007266f7
zuka-e <52391918+zuka-e@users.noreply.github.com>
2023-02-12 23:41:01 +0900

    Configure Playwright for (pre)production env

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: `http://localhost:${PORT}`,
+    baseURL: process.env.APP_URL ?? `http://localhost:${PORT}`,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -86,9 +86,11 @@ export default defineConfig({
   // outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'yarn dev',
-    port: PORT,
-    reuseExistingServer: !process.env.CI,
-  },
+  webServer: process.env.APP_URL
+    ? undefined
+    : {
+        command: 'yarn dev',
+        port: PORT,
+        reuseExistingServer: !process.env.CI,
+      },
 });
