588b3ad087e710d057af0d1f6a338a7839908580
zuka-e <52391918+zuka-e@users.noreply.github.com>
2023-02-23 18:21:55 +0900

    Refactor the name and args of a user event func

--- frontend/test/e2e/register.spec.ts
+++ frontend/test/e2e/register.spec.ts
@@ -7,27 +7,29 @@ test.beforeEach(async ({ page }) => {
   await page.goto('/register');
 });
 
-/** Fill out and submit the form */
-const submitWithInput = async (
-  page: Page,
-  email: string,
-  password: string
-): Promise<void> => {
-  await page.getByRole('textbox', { name: 'Email Address' }).fill(email);
-  await page.getByLabel('Password *').fill(password);
-  await page.getByLabel('Password Confirmation *').fill(password);
-  await page.keyboard.press('Enter');
-};
-
 test.describe('Registration form', () => {
   assertScreenshot();
 
+  /** Fill out and submit the form */
+  const submit = async (
+    page: Page,
+    input: {
+      email: string;
+      password: string;
+    }
+  ): Promise<void> => {
+    const { email, password } = input;
+    await page.getByRole('textbox', { name: 'Email Address' }).fill(email);
+    await page.getByLabel('Password *').fill(password);
+    await page.getByLabel('Password Confirmation *').fill(password);
+    await page.keyboard.press('Enter');
+  };
+
   test('can register', async ({ page }) => {
-    await submitWithInput(
-      page,
-      faker.unique(faker.internet.exampleEmail),
-      'password'
-    );
+    await submit(page, {
+      email: faker.unique(faker.internet.exampleEmail),
+      password: 'password',
+    });
 
     // prettier-ignore
     await Promise.all([
@@ -41,7 +43,7 @@ test.describe('Registration form', () => {
   test('cannot register with existing email', async ({ page }) => {
     const existingEmail = 'test@example.com';
 
-    await submitWithInput(page, existingEmail, 'password');
+    await submit(page, { email: existingEmail, password: 'password' });
 
     // prettier-ignore
     await Promise.all([
