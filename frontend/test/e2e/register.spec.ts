a07fd79256aa6e24586bd0650d894c337a757cb0
zuka-e <52391918+zuka-e@users.noreply.github.com>
2023-02-23 21:23:23 +0900

    Test the registration page as E2E not as component testing

--- frontend/test/e2e/register.spec.ts
+++ frontend/test/e2e/register.spec.ts
@@ -25,6 +25,49 @@ test.describe('Registration form', () => {
     await page.keyboard.press('Enter');
   };
 
+  test('toggles password visibility with checkbox', async ({ page }) => {
+    const formItem = { password: page.getByLabel('Password *') } as const;
+    const passwordDisplayOption = page.getByRole('checkbox', {
+      name: 'Show Password',
+    });
+
+    await expect(formItem.password).not.toHaveAttribute('type', 'text');
+
+    await passwordDisplayOption.click({ force: true });
+
+    expect(await passwordDisplayOption.isChecked()).toBe(true);
+    await expect(formItem.password).toHaveAttribute('type', 'text');
+
+    await passwordDisplayOption.click({ force: true });
+
+    expect(await passwordDisplayOption.isChecked()).toBe(false);
+    await expect(formItem.password).not.toHaveAttribute('type', 'text');
+  });
+
+  test('cannot register with invalid input', async ({ page }) => {
+    const errors = {
+      email: {
+        email: page.getByText('Email Address must be a valid email'),
+      },
+      password: {
+        min: page.getByText('Password must be at least 8 characters'),
+      },
+    };
+
+    await Promise.all([
+      expect(errors.email.email).not.toBeVisible(),
+      expect(errors.password.min).not.toBeVisible(),
+    ]);
+
+    await submit(page, { email: 'test@example', password: 'pass' });
+
+    await Promise.all([
+      expect(page).toHaveURL('/register'),
+      expect(errors.email.email).toBeVisible(),
+      expect(errors.password.min).toBeVisible(),
+    ]);
+  });
+
   test('can register', async ({ page }) => {
     await submit(page, {
       email: faker.unique(faker.internet.exampleEmail),
@@ -50,5 +93,12 @@ test.describe('Registration form', () => {
       expect(page).toHaveURL('/register'),
       expect(page.getByRole('alert').filter({ hasText: /メールアドレスは既に/ })).toBeVisible(),
     ])
+  });
+});
+
+test.describe('links', () => {
+  test('has link to the login page', async ({ page }) => {
+    await page.getByRole('button', { name: 'Sign In' }).click();
+    await expect(page).toHaveURL('/login');
   });
 });
