5425330c670267d739497db37714cb8aa6ad4055
zuka-e <52391918+zuka-e@users.noreply.github.com>
2023-05-08 01:14:20 +0900

    Change the URL transferred after email verification

--- frontend/src/pages/email-verification.tsx
+++ frontend/src/pages/email-verification.tsx
@@ -1,16 +1,12 @@
-import { useEffect } from 'react';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
 import type { GetStaticProps } from 'next';
 
 import { Container, Card, Grid, Typography, Button } from '@mui/material';
 
-import { removeEmailVerificationPage } from '@/store/slices/authSlice';
 import { sendEmailVerificationLink } from '@/store/thunks/auth';
 import { useAppDispatch, useAppSelector } from '@/utils/hooks';
-import { isAfterRegistration, isVerified } from '@/utils/auth';
 import { BaseLayout } from '@/layouts';
-import { AlertMessage } from '@/templates';
+import { AlertMessage, LinkButton } from '@/templates';
 import type { AuthPage } from '@/routes';
 
 type EmailVerificationProps = AuthPage;
@@ -28,23 +24,9 @@ export const getStaticProps: GetStaticProps<
 
 const EmailVerification = () => {
   const dispatch = useAppDispatch();
-  const afterRegistration = useAppSelector(
-    (state) => state.auth.afterRegistration
+  const verified = useAppSelector(
+    (state) => !!state.auth.user?.emailVerifiedAt
   );
-  const router = useRouter();
-
-  // 一時的に表示させるページ
-  // `afterRegistration`: `SignUp`直後に`true` (`replace`しない -> 表示させる)
-  useEffect(() => {
-    if (!isAfterRegistration() || isVerified()) router.replace('/');
-  }, [router, afterRegistration]);
-
-  // DOMアンマウント時に実行 (cleanup function)
-  useEffect(() => {
-    return () => {
-      dispatch(removeEmailVerificationPage());
-    };
-  }, [dispatch]);
 
   const handleClick = () => {
     dispatch(sendEmailVerificationLink());
@@ -61,29 +43,43 @@ const EmailVerification = () => {
           maxWidth="sm"
           className="my-4 flex flex-col gap-4 sm:my-16"
         >
-          <AlertMessage severity="warning" elevation={2} className="font-bold">
-            {`登録から24時間以内に認証を完了させなかった場合、一定時間経過後に登録が抹消されます。`}
+          <AlertMessage
+            severity={verified ? 'success' : 'warning'}
+            elevation={2}
+            className="font-bold"
+          >
+            {verified
+              ? '認証済みです。'
+              : '登録から24時間以内に認証を完了させなかった場合、一定時間経過後に登録が抹消されます。'}
           </AlertMessage>
-          <Card elevation={2} className="p-8">
-            <Grid container direction="column" alignItems="center">
-              <Typography variant="h4" component="h1" gutterBottom>
-                {`認証用メールを送信しました。`}
-              </Typography>
-              <Typography paragraph>
-                {`届いたメールに記載されたURLをクリックして登録を完了させてください。メールが受信できない場合は迷惑メールに振り分けられていないかご確認ください。`}
-              </Typography>
-              <Typography paragraph>
-                {`1時間以内に手続きを行わなかった場合、メールのリンクは無効になります。`}
-              </Typography>
-              <Button
-                variant="contained"
-                color="secondary"
-                onClick={handleClick}
-              >
-                {`メールを再送信する`}
-              </Button>
-            </Grid>
-          </Card>
+          {verified ? (
+            <div>
+              <LinkButton to={'/'} variant="contained" className="w-fit">
+                トップページへ
+              </LinkButton>
+            </div>
+          ) : (
+            <Card elevation={2} className="p-8">
+              <Grid container direction="column" alignItems="center">
+                <Typography variant="h4" component="h1" gutterBottom>
+                  {`認証用メールを送信しました。`}
+                </Typography>
+                <Typography paragraph>
+                  {`届いたメールに記載されたURLをクリックして登録を完了させてください。メールが受信できない場合は迷惑メールに振り分けられていないかご確認ください。`}
+                </Typography>
+                <Typography paragraph>
+                  {`1時間以内に手続きを行わなかった場合、メールのリンクは無効になります。`}
+                </Typography>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  onClick={handleClick}
+                >
+                  {`メールを再送信する`}
+                </Button>
+              </Grid>
+            </Card>
+          )}
         </Container>
       </BaseLayout>
     </>
--- frontend/src/routes/Route.tsx
+++ frontend/src/routes/Route.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
 
 import { verifyEmail } from '@/store/thunks/auth';
-import { useAppDispatch } from '@/utils/hooks';
+import { useAppDispatch, useRoute } from '@/utils/hooks';
 import { AuthRoute, GuestRoute } from '@/routes';
 
 const Route = (
@@ -11,19 +11,29 @@ const Route = (
 ) => {
   const { Component, pageProps } = props;
   const router = useRouter();
+  const route = useRoute();
   const dispatch = useAppDispatch();
 
   useEffect((): void => {
-    const redirectUri = sessionStorage.getItem('redirectUri');
+    (async (): Promise<void> => {
+      if (route.queryParams['verified']?.toString()) {
+        await router.replace('/email-verification');
+        return;
+      }
 
-    if (redirectUri) {
-      sessionStorage.removeItem('redirectUri');
-      dispatch(verifyEmail({ url: redirectUri }));
-    }
-  }, [dispatch, router.asPath]);
+      const redirectUri = sessionStorage.getItem('redirectUri');
 
-  useEffect(() => {
-    return function cleanup() {
+      if (redirectUri) {
+        sessionStorage.removeItem('redirectUri');
+        dispatch(verifyEmail({ url: redirectUri }));
+      }
+    })();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [route.queryParams]);
+
+  useEffect((): (() => void) => {
+    return function cleanup(): void {
       sessionStorage.setItem('previousUrl', router.asPath);
     };
   }, [router.asPath]);
