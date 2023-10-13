8445e3501d83335b452f79021c778a8af157cb55
zuka-e <52391918+zuka-e@users.noreply.github.com>
2023-10-14 00:33:38 +0900

    Suppress MSW unhandled request warnings

--- frontend/test/api/servers/browser.ts
+++ frontend/test/api/servers/browser.ts
@@ -1,6 +1,19 @@
-import { setupWorker } from 'msw';
+import { StartOptions, setupWorker } from 'msw';
 
 import { handlers } from '../handlers';
 
 // This configures a Service Worker with the given request handlers.
 export const worker = setupWorker(...handlers);
+
+/**
+ * @see https://mswjs.io/docs/recipes/debugging-uncaught-requests
+ */
+export const config: StartOptions = {
+  onUnhandledRequest(request, print) {
+    if (request.url.origin !== process.env.NEXT_PUBLIC_API_HOST) {
+      return;
+    }
+
+    print.warning();
+  },
+};
