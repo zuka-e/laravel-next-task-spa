733baa66353922762a8e5099465ded8fa7c19a09
zuka-e <52391918+zuka-e@users.noreply.github.com>
2023-10-14 09:35:00 +0900

    Resolve MSW launching error on the browser

--- frontend/test/api/servers/index.ts
+++ frontend/test/api/servers/index.ts
@@ -1,9 +1,19 @@
 if (typeof window === 'undefined') {
-  const { server } = require('./server');
-  server.listen();
+  // const { server } = require('./server');
+  // server.listen();
+  // ↓ ↓ ↓
+  // If using `server` in (`next dev`), the following error will happen.
+  // `TypeError: Cannot read properties of undefined (reading 'listen')`
+  // In the end, I couldn't find a solution other than removing this.
+  //
+  // Note:
+  // This block runs on the server whose output is displayed on the server console.
+  // On the other hand, `else` block is also executed on the browser, at the same time
+  // (before the first render).
 } else {
-  const { worker } = require('./browser');
-  worker.start();
+  // This block runs on the browser whose output's displayed on the console.
+  const { worker, config: workerConfig } = require('./browser');
+  worker.start(workerConfig);
 }
 
 export {};
