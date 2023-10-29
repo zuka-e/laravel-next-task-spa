0eb55835f448900e038c526564e883e7044eaa06
zuka-e <52391918+zuka-e@users.noreply.github.com>
2023-10-29 20:58:50 +0900

    Declare a type for HTTP methods

--- frontend/src/store/api/utils/isReadRequest.ts
+++ frontend/src/store/api/utils/isReadRequest.ts
@@ -1,10 +1,15 @@
+import { type Method } from 'axios';
+
+type HttpMethod = Uppercase<Method>;
+
 /**
  * Determine if the HTTP request uses a ‘read’ verb.
  *
  * @see https://github.com/laravel/framework/blob/10.x/src/Illuminate/Foundation/Http/Middleware/VerifyCsrfToken.php - isReading()
  */
-const isReadRequest = (method: string): boolean => {
-  return ['HEAD', 'GET', 'OPTIONS'].includes(method.toUpperCase());
+const isReadRequest = (method: HttpMethod): boolean => {
+  const readMethods: HttpMethod[] = ['HEAD', 'GET', 'OPTIONS'];
+  return (readMethods as string[]).includes(method.toUpperCase());
 };
 
 export default isReadRequest;
