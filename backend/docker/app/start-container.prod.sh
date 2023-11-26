2e6e1791d384ee8042050051af53ef3e4ace3539
zuka-e <52391918+zuka-e@users.noreply.github.com>
2023-11-27 03:40:11 +0900

    Optimize for Docker prod

--- backend/docker/app/start-container.prod.sh
+++ backend/docker/app/start-container.prod.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.prod.conf
+php artisan optimize &&
+  exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.prod.conf
+
+#==========================================================================
+# Note:
+#==========================================================================
+#
+# Running `php artisan optimize` in a Dockerfile leads to vulnerabilities,
+# particularly when dealing with sensitive values like `APP_KEY`
+#
