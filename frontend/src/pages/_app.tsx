c241290dd02578d8c2377284d9cb57c26b52c7d0
zuka-e <52391918+zuka-e@users.noreply.github.com>
2024-03-16 17:24:46 +0900

    Output page navigation log for debugging
    cf. https://nextjs.org/docs/pages/api-reference/functions/use-router#routerevents
    cf. https://nextjs.org/docs/app/api-reference/functions/use-router#router-events

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,9 +3,10 @@
 // https://nextjs.org/docs/messages/no-document-viewport-meta
 // e.g. https://github.com/vercel/next.js/blob/canary/examples/with-redux/src/pages/_app.tsx
 
-import { memo } from 'react';
+import { memo, useEffect } from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 import { Provider } from 'react-redux';
 import { DndProvider } from 'react-dnd';
@@ -32,6 +33,38 @@ if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
 }
 
 const App = memo(function App({ Component, pageProps }: AppProps): JSX.Element {
+  const router = useRouter();
+
+  useEffect(() => {
+    // cf. https://nextjs.org/docs/pages/api-reference/functions/use-router#routerevents
+    // cf. https://nextjs.org/docs/app/api-reference/functions/use-router#router-events
+    const handleRouteChangeStart = (
+      url: string,
+      { shallow }: { shallow: boolean }
+    ) => {
+      {
+        console.log(`Navigating to "${url}"${shallow ? ' (shallow)' : ''}.`);
+      }
+    };
+
+    const handleRouteChangeComplete = (
+      url: string,
+      { shallow }: { shallow: boolean }
+    ) => {
+      {
+        console.log(`Navigated to "${url}"${shallow ? ' (shallow)' : ''}.`);
+      }
+    };
+
+    router.events.on('routeChangeStart', handleRouteChangeStart);
+    router.events.on('routeChangeComplete', handleRouteChangeComplete);
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChangeStart);
+      router.events.off('routeChangeComplete', handleRouteChangeComplete);
+    };
+  }, [router]);
+
   return (
     <>
       <Head>
