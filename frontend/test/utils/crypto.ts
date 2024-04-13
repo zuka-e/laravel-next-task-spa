3070f196b36a5b7e9f5b9bc9718503c08930a565
zuka-e <52391918+zuka-e@users.noreply.github.com>
2024-04-13 19:07:45 +0900

    test: Update encryption methods (for study)

--- frontend/test/utils/crypto.ts
+++ frontend/test/utils/crypto.ts
@@ -1,46 +1,92 @@
 import {
+  type BinaryLike,
+  type CipherGCMTypes,
   createCipheriv,
   createDecipheriv,
   createHash,
   pbkdf2Sync,
   randomBytes,
+  scryptSync,
 } from 'crypto';
 
-const CIPHER_ALGORITHM = 'aes-256-cbc';
-const DIGEST_ALGORITHM = 'sha256';
-const PASSWORD = 'd55e76182c856f450ad654f13e2f5b02'; // randomBytes(16).toString('hex')
-const SALT = '8bd861407f94eaf7e7c68150d472119f'; // randomBytes(16).toString('hex')
-/** @see https://nodejs.org/api/crypto.html#cryptopbkdf2syncpassword-salt-iterations-keylen-digest */
-const CIPHER_KEY = pbkdf2Sync(PASSWORD, SALT, 10, 32, DIGEST_ALGORITHM);
+const DIGEST_ALGORITHM = 'sha512';
+
+/**
+ * Derive a key from secrets.
+ *
+ * @param password
+ * @param salt
+ * @param keylen in bytes
+ */
+const deriveKey = (
+  password: BinaryLike,
+  salt: BinaryLike,
+  keylen: number
+): Buffer => {
+  return typeof window === 'undefined'
+    ? // https://nodejs.org/api/crypto.html#cryptoscryptsyncpassword-salt-keylen-options
+      scryptSync(password, salt, keylen)
+    : // https://nodejs.org/api/crypto.html#cryptopbkdf2syncpassword-salt-iterations-keylen-digest
+      pbkdf2Sync(password, salt, 10 ** 4, keylen, DIGEST_ALGORITHM);
+};
+
+const CIPHER_ALGORITHM: CipherGCMTypes = 'aes-256-gcm';
+/** Password to for cipher key */
+// const PASSWORD = 'd55e76182c856f450ad654f13e2f5b02'; // randomBytes(16).toString('hex')
+/** Salt to give the cipher key uniqueness */
+// const SALT = '8bd861407f94eaf7e7c68150d472119f'; // randomBytes(16).toString('hex')
+/** Cipher key length (bytes) which comes from `CIPHER_ALGORITHM` (256bits) */
+// const KEYLEN = 32;
+/** Cipher key string for encryption */
+const APP_KEY =
+  '16991860abf02e87b5d0937eae98ba3f0197a5288a72a1885215abf79845bdb9'; // deriveKey(PASSWORD, SALT, KEYLEN).toString('hex');
+
+/** Cipher key for encryption */
+const CIPHER_KEY = Buffer.from(APP_KEY, 'hex');
+/** Initialization vector length (bytes) */
 const IV_SIZE = 16;
 
 /**
- * @see https://nodejs.org/api/crypto.html#class-cipher - Using the cipher.update() and cipher.final() methods:
+ * Encrypt the string.
  */
 export const encrypt = (text: string): string => {
-  /** Random IV makes an different encrypted value from the same text. */
+  // cf. https://nodejs.org/api/crypto.html#class-cipher - Example: Using the cipher.update() and cipher.final() methods
+
+  // ※ Random IV makes an different encrypted value from the same text.
   const iv = randomBytes(IV_SIZE);
   const cipher = createCipheriv(CIPHER_ALGORITHM, CIPHER_KEY, iv);
-  const encrypted = Buffer.concat([
+  const encryptedText = Buffer.concat([
     cipher.update(text, 'utf8'),
     cipher.final(),
   ]);
-  const encryptedWithIv = Buffer.concat([iv, encrypted]);
+  const authTag = cipher.getAuthTag(); // required for GCM (16bytes)
+  const encrypted = Buffer.concat([iv, encryptedText, authTag]);
 
-  return encryptedWithIv.toString('hex');
+  return encrypted.toString('hex');
 };
 
 /**
- * @see https://nodejs.org/api/crypto.html#class-decipher - Using the decipher.update() and decipher.final() methods
+ * Try decrypting the string and get the result, or `null` on failure.
  */
 export const decrypt = (text: string): string | null => {
+  // cf. https://nodejs.org/api/crypto.html#class-decipher - Example: Using the decipher.update() and decipher.final() methods
+
   try {
-    const encryptedWithIv = Buffer.from(text, 'hex');
-    const iv = encryptedWithIv.subarray(0, IV_SIZE);
-    const encrypted = encryptedWithIv.subarray(IV_SIZE);
+    const encrypted = Buffer.from(text, 'hex');
+
+    const authTagSize = 16; // bytes
+    const authTagPos = encrypted.length - authTagSize;
+
+    const iv = encrypted.subarray(0, IV_SIZE);
+    const encryptedText = encrypted.subarray(IV_SIZE, authTagPos);
+    const authTag = encrypted.subarray(authTagPos);
+
     const decipher = createDecipheriv(CIPHER_ALGORITHM, CIPHER_KEY, iv);
+
+    decipher.setAuthTag(authTag);
+
     const decrypted = Buffer.concat([
-      decipher.update(encrypted),
+      decipher.update(encryptedText),
       decipher.final(),
     ]);
 
@@ -56,4 +102,38 @@ export const decrypt = (text: string): string | null => {
  */
 export const digestText = (text: string): string => {
   return createHash(DIGEST_ALGORITHM).update(text).digest('hex');
+};
+
+/** aka Secret salt (cf. random salt associated with a hash) */
+const PEPPER = '576c8f7fad69e3b194705a9e378dca9c'; // randomBytes(16).toString('hex')
+/** Random salt length (bytes) */
+const SALT_SIZE = 16;
+/** Generated hash length (bytes) */
+const HASH_SIZE = 32;
+
+/**
+ * Generate a hashed value.
+ */
+export const hash = (text: string): string => {
+  const peppered = text + PEPPER;
+  const salt = randomBytes(SALT_SIZE).toString('hex');
+  const hashed = deriveKey(peppered, salt, HASH_SIZE).toString('hex');
+
+  // ※ (random) salt need to be preserved with a hash.
+  return salt + hashed;
+};
+
+/**
+ * Determine if `plain` text could be `hashed` text.
+ */
+export const verifyHash = (plain: string, hashed: string): boolean => {
+  /** Salt length in `hex` */
+  const saltLength = SALT_SIZE * 2;
+  const salt = hashed.substring(0, saltLength);
+  const targetHash = hashed.substring(saltLength);
+  const pepperedPlain = plain + PEPPER;
+  const hashedPlain = deriveKey(pepperedPlain, salt, HASH_SIZE).toString('hex');
+
+  // cf. https://www.php.net/manual/en/function.hash-equals.php
+  return hashedPlain === targetHash;
 };
