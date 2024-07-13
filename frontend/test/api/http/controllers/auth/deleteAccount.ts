31b7a6562f6c5c0fae240bb04d090151fd4b05a1
zuka-e <52391918+zuka-e@users.noreply.github.com>
2024-07-13 13:01:56 +0900

    test: Logout before deleting user according to the real world

--- frontend/test/api/http/controllers/auth/deleteAccount.ts
+++ frontend/test/api/http/controllers/auth/deleteAccount.ts
@@ -3,7 +3,7 @@ import db from '@test/api/database/manager';
 import type { User } from '@test/api/database/models';
 
 export const remove = (user: User) => {
-  db.user.delete({ where: { id: { equals: user.id } }, strict: true });
-
   logout();
+
+  db.user.delete({ where: { id: { equals: user.id } }, strict: true });
 };
