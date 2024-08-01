838f725d6ff114b6e7e2fe8989922f19b12e52ed
zuka-e <52391918+zuka-e@users.noreply.github.com>
2024-08-01 23:18:01 +0900

    chore: Move ESLint to root
    ※ `eslintignore` is not refected if opening the project in a workspace subdirectory

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -5,31 +5,31 @@
 // -- warning  File ignored because of a matching ignore pattern. Use "--no-ignore" to override --
 // cf. https://github.com/okonet/lint-staged#how-can-i-ignore-files-from-eslintignore
 
-// import { ESLint } from 'eslint';
+import { ESLint } from 'eslint';
 
-// /**
-//  * @param {string[]} files
-//  * @returns {Promise<string>}
-//  */
-// const removeIgnoredFiles = async (files) => {
-//   const eslint = new ESLint();
-//   const isIgnored = await Promise.all(
-//     files.map((file) => {
-//       return eslint.isPathIgnored(file);
-//     }),
-//   );
+/**
+ * @param {string[]} files
+ * @returns {Promise<string>}
+ */
+const removeIgnoredFiles = async (files) => {
+  const eslint = new ESLint();
+  const isIgnored = await Promise.all(
+    files.map((file) => {
+      return eslint.isPathIgnored(file);
+    }),
+  );
 
-//   return files.filter((_, i) => !isIgnored[i]).join(' ');
-// };
+  return files.filter((_, i) => !isIgnored[i]).join(' ');
+};
 
 export default {
   // cf. https://github.com/okonet/lint-staged#example-wrap-filenames-in-single-quotes-and-run-once-per-file
   '**/*.{ts,tsx,js,jsx}': async (files) => {
-    // const filesToLint = await removeIgnoredFiles(files);
+    const filesToLint = await removeIgnoredFiles(files);
 
     return [
-      // `eslint --max-warnings=0 ${files}`,
-      `prettier --write ${files}`,
+      `eslint --max-warnings=0 ${filesToLint}`,
+      `prettier --write ${filesToLint}`,
     ];
   },
 };
