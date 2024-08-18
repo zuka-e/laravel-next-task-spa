1cda72d08fecce6ea5f3bd3694fed14277addbb0
zuka-e <52391918+zuka-e@users.noreply.github.com>
2024-08-19 00:12:36 +0900

    chore: Add frontend docker-compose
    ※ `404` if Nginx `root` is `/var/www/html/public`
    
    ※ Cookie can not be set for `localhost` subdomain

--- docker-compose.sh
+++ docker-compose.sh
@@ -17,6 +17,14 @@ resolve_env ".env.docker-compose"
 resolve_env "./backend/.env" &&
   docker compose \
     -f docker-compose.base.yml \
+    -f docker-compose.backend.yml \
     -f docker-compose.dev.yml \
     --profile backend \
     "$@"
+
+docker compose \
+  -f docker-compose.base.yml \
+  -f docker-compose.frontend.yml \
+  -f docker-compose.dev.yml \
+  --profile frontend \
+  "$@"
