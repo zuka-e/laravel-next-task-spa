56c3e77d658ebd5c8e9306408008ee87573c021e
zuka-e <52391918+zuka-e@users.noreply.github.com>
2024-08-23 13:23:16 +0000

    refactor: Split `FortifyServiceProvider` to inner methods

--- backend/app/Providers/FortifyServiceProvider.php
+++ backend/app/Providers/FortifyServiceProvider.php
@@ -47,8 +47,67 @@ class FortifyServiceProvider extends ServiceProvider
     public function boot()
     {
         Fortify::ignoreRoutes();
+
         $this->configureRoutes();
 
+        $this->registerActions();
+
+        $this->registerResponseBindings();
+
+        $this->registerRateLimiters();
+    }
+
+    /**
+     * Configure the routes offered by the application.
+     *
+     * @return void
+     * @see \Laravel\Fortify\FortifyServiceProvider configureRoutes
+     * @see \App\Providers\RouteServiceProvider
+     */
+    protected function configureRoutes()
+    {
+        Route::namespace('Laravel\Fortify\Http\Controllers')
+            ->domain(config('fortify.domain', null))
+            ->prefix(config('fortify.prefix'))
+            ->group(base_path('routes/auth.php'));
+    }
+
+    /**
+     * Register named rate limiters.
+     *
+     * @see https://laravel.com/docs/routing#attaching-rate-limiters-to-routes
+     */
+    protected function registerRateLimiters(): void
+    {
+        RateLimiter::for('login', function (Request $request) {
+            return Limit::perMinute(5)->by($request->email . $request->ip());
+        });
+
+        RateLimiter::for('two-factor', function (Request $request) {
+            return Limit::perMinute(5)->by(
+                $request->session()->get('login.id'),
+            );
+        });
+    }
+
+    /**
+     * Register the actions.
+     */
+    protected function registerActions(): void
+    {
+        Fortify::createUsersUsing(CreateNewUser::class);
+        Fortify::updateUserProfileInformationUsing(
+            UpdateUserProfileInformation::class,
+        );
+        Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
+        Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
+    }
+
+    /**
+     * Register the response bindings.
+     */
+    protected function registerResponseBindings(): void
+    {
         $this->app->singleton(
             RegisterResponseContract::class,
             RegisterResponse::class,
@@ -69,44 +128,5 @@ class FortifyServiceProvider extends ServiceProvider
             LogoutResponseContract::class,
             LogoutResponse::class,
         );
-
-        Fortify::createUsersUsing(CreateNewUser::class);
-        Fortify::updateUserProfileInformationUsing(
-            UpdateUserProfileInformation::class,
-        );
-        Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
-        Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
-
-        /*
-        |----------------------------------------------------------------------
-        | usage: https://laravel.com/docs/routing#attaching-rate-limiters-to-routes
-        | see: `vendor/laravel/fortify/routes/routes.php`
-        |----------------------------------------------------------------------
-        */
-
-        RateLimiter::for('login', function (Request $request) {
-            return Limit::perMinute(5)->by($request->email . $request->ip());
-        });
-
-        RateLimiter::for('two-factor', function (Request $request) {
-            return Limit::perMinute(5)->by(
-                $request->session()->get('login.id'),
-            );
-        });
-    }
-
-    /**
-     * Configure the routes offered by the application.
-     *
-     * @return void
-     * @see \Laravel\Fortify\FortifyServiceProvider configureRoutes
-     * @see \App\Providers\RouteServiceProvider
-     */
-    protected function configureRoutes()
-    {
-        Route::namespace('Laravel\Fortify\Http\Controllers')
-            ->domain(config('fortify.domain', null))
-            ->prefix(config('fortify.prefix'))
-            ->group(base_path('routes/auth.php'));
     }
 }
