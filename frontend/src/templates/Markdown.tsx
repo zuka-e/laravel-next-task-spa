d885495461e013f51336937bc4c8d78f1757bcd7
zuka-e <52391918+zuka-e@users.noreply.github.com>
2025-04-08 06:49:18 +0900

    chore: Update `MarkdownToJsx` to follow `moduleResolution`:`bundler`
    - https://github.com/quantizor/markdown-to-jsx#optionsoverrides---override-any-html-tags-representation

--- frontend/src/templates/Markdown.tsx
+++ frontend/src/templates/Markdown.tsx
@@ -1,43 +1,42 @@
 import { memo, type JSX } from 'react';
 
-import MarkdownToJsx, { MarkdownToJSX } from 'markdown-to-jsx';
+import MarkdownToJsx, { type MarkdownProps } from 'markdown-to-jsx';
 import { Typography } from '@mui/material';
 
-const options: MarkdownToJSX.Options = {
+const OPTIONS = {
   slugify: (str) => str, // 自動生成されるid属性を日本語で利用
   overrides: {
     h1: {
-      component: (props) => (
-        <Typography gutterBottom component="h1" variant="h3" {...props} />
-      ),
+      component: Typography,
+      props: { gutterBottom: true, component: 'h1', variant: 'h3' },
     },
     h2: {
-      component: (props) => (
-        <Typography gutterBottom component="h2" variant="h4" {...props} />
-      ),
+      component: Typography,
+      props: { gutterBottom: true, component: 'h2', variant: 'h4' },
     },
     h3: {
-      component: (props) => (
-        <Typography gutterBottom component="h3" variant="h5" {...props} />
-      ),
+      component: Typography,
+      props: { gutterBottom: true, component: 'h3', variant: 'h5' },
     },
     h4: {
-      component: (props) => (
-        <Typography gutterBottom component="h4" variant="h6" {...props} />
-      ),
+      component: Typography,
+      props: { gutterBottom: true, component: 'h4', variant: 'h6' },
     },
-    p: { component: (props) => <Typography paragraph {...props} /> },
+    p: { component: Typography, props: { paragraph: true } },
     ol: { props: { style: { paddingInlineStart: '1.6rem' } } },
-    li: {
-      component: (props) => <Typography component="li" {...props} />,
-    },
+    li: { component: Typography, props: { component: 'li' } },
   },
-};
+} as const satisfies MarkdownProps['options'];
 
 const Markdown = memo(function Markdown({
   children,
-}: Parameters<typeof MarkdownToJsx>[0]): JSX.Element {
-  return <MarkdownToJsx options={options}>{children}</MarkdownToJsx>;
+  options,
+}: MarkdownProps): JSX.Element {
+  return (
+    <MarkdownToJsx options={{ ...OPTIONS, ...options }}>
+      {children}
+    </MarkdownToJsx>
+  );
 });
 
 export default Markdown;
